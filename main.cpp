6301570001c682eae366dfb578edb7fe4f0dd06e
Uday Patel <udaykpatel@hotmail.com>
2017-11-18 15:07:51 +0000

    Fixed issue with ostream

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main()
 {
     Pod pod;
     std::cout << "Name: " <<pod.get_name() << std::endl;
-    std::cout << "Maximum speed: " << pod.get_max_speed() << std::end;
+    std::cout << "Maximum speed: " << pod.get_max_speed() << std::endl;
     std::cout << "Temperature: " << pod.get_temperature() << std::endl;
     std::cout << "Pressure: " << pod.get_pressure() << std::endl;
     return 0;
