f0e58602b9e20c0e7aa1a171f18ada2373dbdf8a
Uday Patel <udaykpatel@hotmail.com>
2017-11-18 15:05:26 +0000

    Changed values to proceed to next task

--- pod.cpp
+++ pod.cpp
@@ -25,11 +25,11 @@ double Pod::get_max_speed()
 /// TASKS 3 (PERSON A) & 4 (PERSON B) //////////////////////////////////////////
 double Pod::get_temperature()
 {
-  return 0;
+  return 1;
 }
 
 double Pod::get_pressure()
 {
-  return 0;
+  return 1;
 }
 /// END OF TASKS 3A & 4B ///////////////////////////////////////////////////////
