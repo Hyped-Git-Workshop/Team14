525e064150c8e6e75e670b731a7ebf2449ef861c
Uday Patel <udaykpatel@hotmail.com>
2017-11-18 14:48:37 +0000

    Created function for pressure

--- pod.cpp
+++ pod.cpp
@@ -23,5 +23,13 @@ double Pod::get_max_speed()
 }
 
 /// TASKS 3 (PERSON A) & 4 (PERSON B) //////////////////////////////////////////
+double Pod::get_temperature()
+{
+  return 0;
+}
 
+double Pod::get_pressure()
+{
+  return 0;
+}
 /// END OF TASKS 3A & 4B ///////////////////////////////////////////////////////
